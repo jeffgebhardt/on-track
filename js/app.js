b1e30eec49da5804f7afd341448e686bc7f0c705
sbuckley <sbuckley65@hotmail.com>
2016-05-02 15:31:23 -0700

    got array of quotes done

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
-var quotes;
-var celeb;
+var allQuotes = [];
+var appendFooterTwo = document.getElementById('footerTwo');
 
-
-function RandomQuotes(said,who) {
+function Randomquotes(quote,who) {
   this.quote = quote;
   this.who = who;
+  allQuotes.push(this);
 }
 
-var quote1 = new RandomQuotes('Strength is born in the deep silence of long-suffering hearts; not amidst joy.','Felicia Hemans');
+var quote1 = new Randomquotes('Strength is born in the deep silence of long-suffering hearts; not amidst joy.','Felicia Hemans');
 
-var quote2 = new RandomQuotes('When you come to the end of your rope, tie a knot and hang on.','Franklin D. Roosevelt');
+var quote2 = new Randomquotes('When you come to the end of your rope, tie a knot and hang on.','Franklin D. Roosevelt');
 
-var quote3 = new RandomQuotes('You can be a victim of cancer, or a survivor of cancer. It’s a mindset','Dave Pelzer');
+var quote3 = new Randomquotes('You can be a victim of cancer, or a survivor of cancer. It’s a mindset','Dave Pelzer');
 
-var quote4 = new RandomQuotes('Cancer may have started the fight, but I will finish it.','gotCancer.org');
+var quote4 = new Randomquotes('Cancer may have started the fight, but I will finish it.','gotCancer.org');
 
-var quote5 = new RandomQuotes('You never know how strong you are until being strong is the only choice you have.','Cayla Mills');
+var quote5 = new Randomquotes('You never know how strong you are until being strong is the only choice you have.','Cayla Mills');
 
 function randomNumber(min,max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function randomQuotePicker() {
-  return productInfos[randomNumber(0,productInfos.length - 1)];
+  return allQuotes[randomNumber(0,allQuotes.length - 1)];
 }
