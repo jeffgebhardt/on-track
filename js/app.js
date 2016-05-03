d13daa12f835dd9df4684a0446f3138bd3edba21
sbuckley <sbuckley65@hotmail.com>
2016-05-03 09:28:17 -0700

    making sure up to date

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
 var allQuotes = [];
 var newQuote;
 
-function Randomquotes(quote,who) {
+function Randomquotes(quote) {
   this.quote = quote;
-  this.who = who;
   allQuotes.push(this);
 }
 
-var quote1 = new Randomquotes('Strength is born in the deep silence of long-suffering hearts; not amidst joy.','Felicia Hemans');
+var quote1 = new Randomquotes('"Strength is born in the deep silence of long-suffering hearts; not amidst joy." - Felicia Hemans');
 
-var quote2 = new Randomquotes('When you come to the end of your rope, tie a knot and hang on.','Franklin D. Roosevelt');
+var quote2 = new Randomquotes('"When you come to the end of your rope, tie a knot and hang on." - Franklin D. Roosevelt');
 
-var quote3 = new Randomquotes('You can be a victim of cancer, or a survivor of cancer. It’s a mindset','Dave Pelzer');
+var quote3 = new Randomquotes('"You can be a victim of cancer, or a survivor of cancer. It’s a mindset." - Dave Pelzer');
 
-var quote4 = new Randomquotes('Cancer may have started the fight, but I will finish it.','gotCancer.org');
+var quote4 = new Randomquotes('"Cancer may have started the fight, but I will finish it." - gotCancer.org');
 
-var quote5 = new Randomquotes('You never know how strong you are until being strong is the only choice you have.','Cayla Mills');
+var quote5 = new Randomquotes('"You never know how strong you are until being strong is the only choice you have." - Cayla Mills');
 
 function randomNumber(min,max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -28,10 +27,10 @@ function randomQuotePicker() {
 
 function addQuote(quoteText) {
   randomQuotePicker();
-  var displayQuote = document.getElementById('footerTwo');
-  var pEl = document.createElement('p');
-  pEl.textContent = newQuote;
-  displayQuote.appendChild('p');
+  // var displayQuote = document.getElementById('footerTwo');
+  // var pEl = document.createElement('p');
+  document.getElementById('quotePara').textContent = newQuote;
+  // displayQuote.appendChild('p');
 }
 
 addQuote();
