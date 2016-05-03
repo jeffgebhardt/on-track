0214364a875857d6f9301dad9263ccd3df6fa946
sbuckley <sbuckley65@hotmail.com>
2016-05-03 15:00:26 -0700

    removed canvas and began animation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,1 @@
-var welcome = document.getElementById('welcomeTo');
-var ctx = welcome.getContext('2d');
-ctx.font = '75px serif';
-ctx.fillText('Welcome to',50,50);
-ctx.textAlign = 'left';
-ctx.textBaseline = 'middle';
+document.onload = function 
