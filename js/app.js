5861a1ded7a8ce1f8a4d3f71f4c1b1ec49ac9884
sbuckley <sbuckley65@hotmail.com>
2016-05-04 11:56:42 -0700

    animated header, began work on picture switch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-document.onload = function 
+var currentUser = new User();
+
+function switchPicture() {
+  var patientPicture = document.getElementsById('patient-picture').src = 'img/activepatient.png';
+}
+
+function timeSwitch() {
+  setInterval(switchPicture, 3000);
+}
+
+if (localStorage.getItem('OnTrack-currentUser')){
+  console.log('Local Storage Exists');
+  currentUser.getUserDataFromStorage();
+  window.open('daily.html', '_self');
+}
+
+timeSwitch();
