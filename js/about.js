66462d7ff8595fe38ee4c0b7ee99595fcf772850
Jeffrey Gebhardt <jeff@gebbey.com>
2016-05-05 11:54:30 -0700

    much good, so wow

--- js/about.js
+++ js/about.js
@@ -10,4 +10,16 @@ function greetUser(){
   return capital;
 };
 
+function buttonHandler(e) {
+  var targetEl = e.target;
+  e.preventDefault();
+  switch (e.target.id) {
+  case 'signout-button':
+    currentUser.userSignedOut();
+    break;
+
+  }
+}
+document.getElementById('signout-button').addEventListener('click',buttonHandler, false);
+
 document.getElementById('helloMessage').innerHTML = 'Hello ' + greetUser() + '.';
